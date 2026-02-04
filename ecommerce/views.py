fe84cc2d7ea4f74e5dc62ce6402c3e569f65fe7e
Shamsudeenmohammed <mshamsudeen551@gmail.com>
2026-02-04 10:23:32 +0000

    add all latest updates of the project

--- ecommerce/views.py
+++ ecommerce/views.py
@@ -1,19 +1,24 @@
 from django.shortcuts import render
 from products.models import Product, Category
+from django.db.utils import OperationalError
 
 def home_view(request):
-    featured_products = Product.objects.filter(
-        is_featured=True,
-        is_active=True
-    )[:8]
+    try:
+        featured_products = Product.objects.filter(
+            is_featured=True,
+            is_active=True
+        )[:8]
 
-    categories = Category.objects.filter(
-        is_active=True,
-        parent__isnull=True
-    )
+        categories = Category.objects.filter(
+            is_active=True,
+            parent__isnull=True
+        )
 
-    context = {
+    except OperationalError:
+        featured_products = []
+        categories = []
+
+    return render(request, 'home.html', {
         'featured_products': featured_products,
         'categories': categories,
-    }
-    return render(request, 'home.html', context)
+    })
